21742aeb9d2ec6fa1c2ad6d28268c1eb5a1ea405
Yves Riedener <git@reecube.com>
2018-01-05 13:40:09 +0100

    WIP

--- convert.php
+++ convert.php
@@ -16,11 +16,74 @@ function readFiles(string $relativePath, string $extension): array
             continue;
         }
 
-        $files[] = $file;
+        $files[] = realpath($relativePath . DIRECTORY_SEPARATOR . $file);
     }
 
     return $files;
 }
 
-var_dump(readFiles($PATH_INPUT, 'txt'));
-var_dump(readFiles($PATH_STYLE, 'css'));
+function groupMatches(array $matches): array
+{
+    $result = [];
+    $persons = [
+        '' => 0,
+    ];
+
+    foreach ($matches as $match) {
+        $date = $match[1];
+
+        if (!isset($result[$date])) {
+            $result[$date] = [];
+        }
+
+        $idx = strpos($match[3], ': ');
+
+        if ($idx === false) {
+            $idx = 0;
+        }
+
+        $time = $match[2];
+        $person = substr($match[3], 0, $idx);
+        $message = substr($match[3], $idx);
+
+        if (!isset($persons[$person])) {
+            $persons[$person] = count($persons);
+        }
+
+        $result[$date][] = [
+            'time' => $time,
+            'person' => $person,
+            'personId' => $persons[$person],
+            'message' => $message,
+        ];
+    }
+
+    return $result;
+}
+
+foreach (readFiles($PATH_INPUT, 'txt') as $file) {
+    $content = file_get_contents($file);
+
+    // fix invalid html
+    $content = preg_replace('/<(.*?)>/', '<span class="special">\1</span>', $content);
+
+    // fix lines and linebreaks
+    $content = preg_replace('/\r\n/', "\r", $content);
+    $content = preg_replace('/\n/', '<br>', $content);
+    $content = preg_replace('/\r/', "\r\n", $content);
+
+    // fix quotes
+    $content = preg_replace('/"/', '&quot;', $content);
+
+    // parse data
+    $matches = [];
+    $result = preg_match_all('/(.*?),\s(.*?):\s(.*?)\r\n/', $content, $matches, PREG_SET_ORDER);
+
+    if ($result === false) {
+        throw new Exception('Unexpected result!');
+    }
+
+    $groups = groupMatches($matches);
+
+    var_dump($groups);
+}
