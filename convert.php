7abdcaf59b4585200c01251f628e33e869256cc7
Yves Riedener <git@reecube.com>
2018-01-05 12:52:21 +0100

    WIP

--- convert.php
+++ convert.php
@@ -1,3 +1,26 @@
 <?php
 
-echo 'TODO';
+$PATH_BASE = __DIR__ . DIRECTORY_SEPARATOR;
+$PATH_INPUT = $PATH_BASE . 'in';
+$PATH_STYLE = $PATH_BASE . 'style';
+
+function readFiles(string $relativePath, string $extension): array
+{
+    $files = [];
+
+    foreach (scandir($relativePath) as $file) {
+        $ext = pathinfo($file, PATHINFO_EXTENSION);
+        $ext = strtolower($ext);
+
+        if ($ext !== $extension) {
+            continue;
+        }
+
+        $files[] = $file;
+    }
+
+    return $files;
+}
+
+var_dump(readFiles($PATH_INPUT, 'txt'));
+var_dump(readFiles($PATH_STYLE, 'css'));
