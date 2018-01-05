4d502317834cd8ba11775e502734022393ba2bbd
Yves Riedener <git@reecube.com>
2018-01-05 18:14:26 +0100

    fixed emoij support

--- convert.php
+++ convert.php
@@ -69,20 +69,12 @@ function addEmojiSupport(string &$html): string
     $html .= '<script src="https://twemoji.maxcdn.com/twemoji.min.js"></script>';
     $html .= '<script>
 window.onload = function() {
-
-  // Set the size of the rendered Emojis
-  // This can be set to 16x16, 36x36, or 72x72
   twemoji.size = \'16x16\';
-
-  // Parse the document body and
-  // insert <img> tags in place of Unicode Emojis
   twemoji.parse(document.body);
-
 }
 </script>';
     $html .= '<style>
 img.emoji {  
-  // Override any img styles to ensure Emojis are displayed inline
   margin: 0 1px !important;
   display: inline !important;
 }
