84fa7dad80ebd724d922c32252fa6cc284e11f4b
Yves Riedener <git@reecube.com>
2018-01-05 17:46:34 +0100

    added emoji support (also for print)

--- convert.php
+++ convert.php
@@ -64,6 +64,33 @@ function groupMatches(array $matches): array
     return $result;
 }
 
+function addEmojiSupport(string &$html): string
+{
+    $html .= '<script src="https://twemoji.maxcdn.com/twemoji.min.js"></script>';
+    $html .= '<script>
+window.onload = function() {
+
+  // Set the size of the rendered Emojis
+  // This can be set to 16x16, 36x36, or 72x72
+  twemoji.size = \'16x16\';
+
+  // Parse the document body and
+  // insert <img> tags in place of Unicode Emojis
+  twemoji.parse(document.body);
+
+}
+</script>';
+    $html .= '<style>
+img.emoji {  
+  // Override any img styles to ensure Emojis are displayed inline
+  margin: 0 1px !important;
+  display: inline !important;
+}
+</style>';
+
+    return $html;
+}
+
 function generateHtml(array $groups, string $style, string $title = null): string
 {
     $result = '';
@@ -104,6 +131,8 @@ function generateHtml(array $groups, string $style, string $title = null): strin
 
         $result .= '</div>';
     }
+
+    addEmojiSupport($result);
 
     $result .= '</body>';
     $result .= '</html>';
